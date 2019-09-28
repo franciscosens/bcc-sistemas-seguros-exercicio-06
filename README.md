f3ad09e6ae14880b8ab34a147924a2a7ebfb77bf
Francisco Lucas Sens <francisco.lucas.sens@gmail.com>
2019-09-27 21:39:39 -0300

    Desenvolvido a questão

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ Alunos:
 * Gabriel Castellani de Oliveira
 
 # Enunciado: 
+Implemente o algoritmo de criptografia e decriptografia conhecido como cifra de Vigenère.
+
+O programa deve possibilitar que o usuário selecione a operação a ser executada, entre cifragem e decifragem. Além disso, o programa deve possibilitar que o usuário informe a chave a ser utilizada no algoritmo.
+
+Da mesma forma que a cifra de César, a cifra de Vigenère somente é aplicável aos caracteres do alfabeto (de A à Z).
+
+Espaços e outros caracteres especiais não fazem parte do texto simples.
+
+Para evidenciar o resultado deste exercício. Publique no AVA:
+
+Um print screen da tela do programa, contendo um texto simples, o texto cifrado equivalente e a chave utilizada.
--- questao01.py
+++ questao01.py
@@ -3,11 +3,11 @@ import re
 # Cifra de Vigenère
 
 class ConversorCifraVesemir:
-    def __init__(self):
-        pass
+    def __init__(self, caracteres):
+        self.caracteres = caracteres
 
-    def cifrar(self, texto, caracteres):
-        if not re.fullmatch(r'([a-zA-Z\t]*)', texto):
+    def cifrar(self, texto):
+        if not re.fullmatch(r'([a-zA-Z\s]*)', texto):
             raise Exception("Texto deve conter somente letras do alfabeto")
         letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
         indice = 0
@@ -16,34 +16,63 @@ class ConversorCifraVesemir:
             letra = texto[i]
             letraAux = letra.upper()
             codigo_ascii_letra = ord(letraAux)
-            valor_letra = ord(caracteres[indice]) - 65
-            if codigo_ascii_letra + valor_letra > 90:
+            valor_letra = ord(self.caracteres[indice]) - 65
+            if codigo_ascii_letra == 32:
+                codigo_ascii_letra_nova = 32
+            elif codigo_ascii_letra + valor_letra > 90:
                 codigo_ascii_letra_nova = 65 + (((90 - codigo_ascii_letra) - valor_letra) * -1) - 1
             elif codigo_ascii_letra + valor_letra > 122:
                 codigo_ascii_letra_nova = 97 + (((122 - codigo_ascii_letra) - valor_letra) * -1) - 1
             else:
                 codigo_ascii_letra_nova = codigo_ascii_letra +  valor_letra
-
-            print(str(codigo_ascii_letra)  + ' - ' + str(codigo_ascii_letra)  + ' - ' + str(codigo_ascii_letra_nova) + ' - ' + str(valor_letra)) 
             cifrado = chr(codigo_ascii_letra_nova)
             cifra = cifra + cifrado
-            print(letra + '  -  ' + cifrado)
-            if(indice + 1 >= len(caracteres)):
+            if indice + 1 >= len(self.caracteres):
                 indice = 0
             else:
                 indice = indice + 1
         return cifra
 
     def decifrar(self, cifra):
-        pass
+        texto = ''
+        indice = 0
+        quantidade_caracteres = len(cifra)
+        for i in range(0, quantidade_caracteres):
+            letra = cifra[i].upper()
+            letra_codigo_ascii = ord(letra)
+
+            letra_caracter = self.caracteres[indice]
+            letra_caracter_codigo_ascii = ord(letra_caracter)
+            valor_letra = letra_caracter_codigo_ascii - 65
+            
+            if letra_codigo_ascii == 32:
+                nova_letra_codigo_ascii = 32
+            elif letra_codigo_ascii - valor_letra < 65:
+                nova_letra_codigo_ascii = 90 - (letra_caracter_codigo_ascii - letra_codigo_ascii) + 1
+            else:
+                nova_letra_codigo_ascii = letra_codigo_ascii - valor_letra
+            nova_letra = chr(nova_letra_codigo_ascii)
+            texto = texto + nova_letra
+            indice = 0 if indice + 1 >= len(self.caracteres) else indice + 1
+        return texto
 
 if __name__ == "__main__":
-    conversor = ConversorCifraVesemir()
-    # cifrado = conversor.cifrar("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
-    cifrado = conversor.cifrar("abcdefghijklmnopqrstuvwxyz", ['D', 'U', 'H'])
-    cifrado = conversor.cifrar("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", ['D', 'U', 'H'])
-    print(cifrado)
-    # texto = conversor.decifrar(cifrado)
-    # print(texto)
 
-    
+    opcao = 1
+    print('Informe os caracteres separando com espaço os caracteres: ', end='')
+    chave = input().split(' ')
+    conversor = ConversorCifraVesemir(chave)
+    while opcao != 3:
+
+        print('1 - Cifrar\n2 - Decrifrar\n3 - Sair')
+        opcao = int(input())
+        if opcao == 1:
+            print('Digite o texto para cifrar: ', end='')
+            texto = input()
+            cifrado = conversor.cifrar(texto)
+            print('Cifrado: ' + cifrado)
+        elif opcao == 2:
+            print('Digite o texto para decifrar: ', end='')
+            cifrado = input()
+            texto = conversor.decifrar(cifrado)
+            print('Cifrado: ' + texto)
